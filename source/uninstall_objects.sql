cc1a912ab48974e69a70e19018fb503f62f78b5e
Jacek Gebal <jgebal@users.noreply.github.com>
2019-03-15 20:04:15 +0000

    Adding trigger solution for annotation parsing.

--- source/uninstall_objects.sql
+++ source/uninstall_objects.sql
@@ -161,6 +161,8 @@ drop table ut_compound_data_tmp;
 
 drop table ut_compound_data_diff_tmp;
 
+drop trigger ut_trigger_annotation_parsing;
+
 drop package ut_annotation_manager;
 
 drop package ut_annotation_parser;
@@ -170,6 +172,8 @@ drop package ut_annotation_cache_manager;
 drop table ut_annotation_cache cascade constraints;
 
 drop table ut_annotation_cache_info cascade constraints;
+
+drop table ut_annotation_cache_schema cascade constraints;
 
 drop sequence ut_annotation_cache_seq;
 
@@ -184,6 +188,8 @@ drop type ut_annotated_object force;
 drop type ut_annotations force;
 
 drop type ut_annotation force;
+
+drop package ut_trigger_check;
 
 drop package ut_file_mapper;
 
